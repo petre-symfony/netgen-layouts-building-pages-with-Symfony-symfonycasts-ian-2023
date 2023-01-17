5b531c3d07bb4be55a8e79e20940b6ca1c67e1fc
petrero <petrero@gmail.com>
2023-01-17 23:00:10 +0200

    13.1 So our content browser was working beautifully... until we selected an item. At that time, it chose to do an odd thing: explode! The Ajax call that failed says:
      Value loader for doctrine_recipe value type does not exist.
    
     To review: we have a custom value type called doctrine_recipe, which we created so that we could add grids and lists of Recipe entities. For this to work, we have (1): a value converter to convert Recipe objects into a format understood by layouts. (2) a query type to allow us to use dynamic collections. (3) a browser backend class to allow us to select manual items. And now (4), we need a value loader that is able to take the "id" of these manually-selected items and turn them into Recipe objects. This will be the last "thing" we need for our value type, I promise!
    
     Creating & Tagging the Value Loader
     - Inside the src/Layouts/ directory, create a new class called RecipeValueLoader, make it implement ValueLoaderInterface and generate the two methods it needs
    
     These are pretty simple. But, before we fill them in, go back to the Ajax endpoint, and refresh to see... the exact same error. Why? Like we've seen with other things, we need to "associate" this RecipeValueLoader with our doctrine_recipe value type. How? No surprise! With a tag. Say #[AutoconfigureTag()] and this time it's called netgen_layouts.cms_value_loader. For the second argument, pass value_type set to doctrine_recipe
    
     Perfecto! If we reload now... better! That error is because we haven't actually filled in the logic yet.

---
+++ src/Layouts/RecipeValueLoader.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Layouts;
+
+use Netgen\Layouts\Item\ValueLoaderInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('netgen_layouts.cms_value_loader', ['value_type' => 'doctrine_recipe'])]
+class RecipeValueLoader implements ValueLoaderInterface {
+	public function load($id): ?object {
+		// TODO: Implement load() method.
+	}
+
+	public function loadByRemoteId($remoteId): ?object {
+		// TODO: Implement loadByRemoteId() method.
+	}
+}
